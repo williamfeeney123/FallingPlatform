e03ca5548757648a7f1b9b5c96ea9c5853e78a46
Squill <williamfeeney123@gmail.com>
2020-09-01 23:57:28 -0400

    Commit
    Commit

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    public bool theyCollided;
+    public float speed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theyCollided = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (theyCollided == true)
+        {
+            transform.position += new Vector3(0f, -speed, 0f) * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Fall");
+        theyCollided = true;
+    }
+}
